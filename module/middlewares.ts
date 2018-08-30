82d73231b315964d072241aaaa669e47b07f7706
bangbang93 <bangbang93@163.com>
2018-08-30 18:00:09 +0800

    优化

--- module/middlewares.ts
+++ module/middlewares.ts
@@ -1,5 +1,6 @@
 import * as bunyan from 'bunyan'
 import {NextFunction, Request, Response} from 'express'
+import {validationResult} from 'express-validator/check'
 import {logger as loggerConfig} from '../config'
 
 const Logger = bunyan.createLogger(loggerConfig.middleware as any)
@@ -29,5 +30,14 @@ export function haruhiMiddleware(req: Request, res: Response, next: NextFunction
       })
   }
   req.logger = Logger.child({req})
+  next()
+}
+
+export function checkValidationResult(req: Request, res: Response, next: NextFunction) {
+  const validation = validationResult(req)
+  if (!validation.isEmpty()) {
+    return res.json(400)
+      .json(validation.mapped())
+  }
   next()
 }
