aa41ebc9eab3d2825cb8956130306bd16cfbfe4c
bangbang93 <bangbang93@163.com>
2018-08-30 11:53:07 +0800

    use bcrypt for password

--- service/user.ts
+++ service/user.ts
@@ -1,18 +1,18 @@
 /**
  * Created by bangbang93 on 2017/3/1.
  */
-import * as HashHelper from '../helper/hash'
+import * as bcrypt from 'bcrypt'
 import {UserModel} from '../model/user'
 
 export class UserService {
   public static async login(username: string, password: string) {
-    const user = await UserModel.getByUsername(username)
+    const user = await UserModel.findOne({username})
+      .select('+password')
     if (!user) throw new Error('no such user')
 
-    const pwd = HashHelper.password(username, password)
-    if (pwd !== user.password) throw new Error('wrong password')
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+    if (!isPasswordCorrect) throw new Error('wrong password')
 
-    delete user.password
     return user
   }
 }
